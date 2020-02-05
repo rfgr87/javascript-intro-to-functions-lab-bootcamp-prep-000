d99488ebeb093f21c163a71f9f4c45b00b97b6a6
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-05 00:55:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ function logShout(string){
   console.log(string.toUpperCase())
 }
 
-function sayHiToGrandma(string){
-  if (string === string.toUpperCase()){
-    
-  }
-  
-}
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
+function sayHiToGrandma(string){
+  if (string === string.toUpperCase()){
+    returns "YES INDEED"
+  }
+  else{
+    returns 
+  }
+  
+}
